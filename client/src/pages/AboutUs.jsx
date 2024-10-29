e761c3ea7977270273af4c29209fcf6ceedde706
batmanknows <122247678+batmnnn@users.noreply.github.com>
2024-10-29 15:10:37 +0530

    mm

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import adarsh from '../assets/adarsh.png'
+import shubham from '../assets/shubham.jpeg'
 import shivam from '../assets/shivam.jpg'
 
 
@@ -24,16 +24,16 @@ const AboutUs = () => {
             <section className="mb-12">
                 <h2 className="text-2xl  md:text-3xl font-bold mb-4">Meet the Founders</h2>
                 <div className="flex items-center mb-6">
-                    <img src={adarsh} className="w-24 h-22 rounded-full" alt="Adarsh Singh" />
+                    <img src={shubham} className="w-24 h-22 rounded-full" alt="Adarsh Singh" />
                     <div className="ml-4">
-                        <h3 className="text-xl font-semibold">Adarsh Singh</h3>
+                        <h3 className="text-xl font-semibold">Shubham Yadav</h3>
                         <p className="text-gray-700">Founder</p>
                     </div>
                 </div>
                 <div className="flex items-center">
                     <img src={shivam} className="w-24 h-22 rounded-full" alt="Shivam Verma" />
                     <div className="ml-4">
-                        <h3 className="text-xl font-semibold">Shivam Verma</h3>
+                        <h3 className="text-xl font-semibold">Swastik Tiwari</h3>
                         <p className="text-gray-700">Co-founder</p>
                     </div>
                 </div>
